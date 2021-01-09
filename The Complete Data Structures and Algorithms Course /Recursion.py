1d5c8c79801d7d6343bdce20ceff6b71e3ef56fd
Programming-Skills <hewingmartin@gmail.com>
2021-01-09 18:27:04 +0000

    Updated files

---
+++ The Complete Data Structures and Algorithms Course /Recursion.py
@@ -0,0 +1,62 @@
+# Recursion.py
+
+# Recursion = a way of solving a problem by having a function call itself.
+
+def openRussianDoll(doll):
+    if doll == 1:
+        print("All dolls are opened")
+    else:
+        openRussianDoll(doll-1)
+
+# How recursion works?
+# 1. A method calls itself 
+# 2. Exit from the infinite loop 
+
+def recursionMethod(parameters):
+    if exit from condition satified:
+        return some value
+    else:
+        recursionMethod(modified parameters)
+
+def recursiveMethod(n):
+    if n<1:
+        print("n is less than one")
+    else:
+        recursionMethod(n-1)
+        print(n)
+
+# Recursive
+
+def powerOfTwo(n):
+    if n == 0:
+        return 1
+    else:
+        power = powerOfTwo(n-1)
+        return power * 2
+
+# Iterative
+
+def powerOfTwo(n):
+    i = 0
+    power = 1
+    while i < n:
+        power = power * 2
+        i = i + 1
+    return power
+
+# How to write recursion in 3 steps?
+
+# 1. Recursive case - the flow
+
+# n! = n * (n-1) * (n-2) * ... * 2 * 1 -> n! = n * (n-1)!
+
+# 2. Base case - the stopping criterion
+
+# 0! = 1
+# 1! = 1
+
+def factorial(n):
+    if n in [0, 1]:
+        return 1
+    else:
+        return n * factorial(n-1)
--- The Complete Data Structures and Algorithms Course /recursiveRange.py
+++ The Complete Data Structures and Algorithms Course /recursiveRange.py
@@ -3,16 +3,13 @@
 import argparse
 
 def recursiveRange(num):
-    container = []
     if num == 0:
-        return 1
+        return 0
     else:
-        for i in list(range(num)):
-            container.append(i)
-        return (container[0] + recursiveRange(container[1:]))
+        return (num + recursiveRange(num-1))
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description=__doc__)
+    parser = argparse.ArgumentParser(description = __doc__)
     parser.add_argument('num', type=int)
     args = parser.parse_args()
     print("File executed")
